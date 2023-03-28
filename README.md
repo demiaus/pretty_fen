a2cb3f7e9d123bcf5ffc69f5679ae4c667554d70
demiaus <demiaus@amideus.fi>
2023-03-28 19:16:01 +0300

    WIP

--- README.md
+++ README.md
@@ -2,19 +2,23 @@
 
 Format Forsyth–Edwards Notation into pretty human-readable form:
 ```
- r n b q k b n r /
- p p p p p p p p /
- . . . . . . . . /
- . . . . . . . . /
- . . . . . . . . /
- . . . . . . . . /
- P P P P P P P P /
- R N B Q K B N R
+ r  n  b  q  k  b  n  r  /
+ p  p  p  p  p  p  p  p  /
+ .  .  .  .  .  .  .  .  /
+ .  .  .  .  .  .  .  .  /
+ .  .  .  .  .  .  .  .  /
+ .  .  .  .  .  .  .  .  /
+ P  P  P  P  P  P  P  P  /
+ R  N  B  Q  K  B  N  R
 ```
 
 This preserves only the board position and discards castling etc. information.
 
 ## Example usage
+compile:
+```
+rustc -O -o pretty_fen src/main.rs
+```
 ```
 ./pretty_fen "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
 ```
@@ -22,29 +26,32 @@ This preserves only the board position and discards castling etc. information.
 ./pretty_fen "rnbqkbnr/pppppppp/8/8/4(P)3/8/PPPP(.)PPP/RNBQKBNR w KQkq - 0 1"
 ```
 ```
- r n b q k b n r /
- p p p p p p p p /
- . . . . . . . . /
- . . . . . . . . /
- . . . .(P). . . /
- . . . . . . . . /
- P P P P(.)P P P /
- R N B Q K B N R
+ r  n  b  q  k  b  n  r  /
+ p  p  p  p  p  p  p  p  /
+ .  .  .  .  .  .  .  .  /
+ .  .  .  .  .  .  .  .  /
+ .  .  .  . (P) .  .  .  /
+ .  .  .  .  .  .  .  .  /
+ P  P  P  P (.) P  P  P  /
+ R  N  B  Q  K  B  N  R
  ```
 Supports highlighting using parentheses:
 ```
 ./pretty_fen "rnbqkbnr/pppppppp/8/2(4)2/2(4)2/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
 ```
 ```
- r n b q k b n r /
- p p p p p p p p /
- . . . . . . . . /
- . .(. . . .). . /
- . .(. . . .). . /
- . . . . . . . . /
- P P P P P P P P /
- R N B Q K B N R
+ r  n  b  q  k  b  n  r  /
+ p  p  p  p  p  p  p  p  /
+ .  .  .  .  .  .  .  .  /
+ .  . (.  .  .  .) .  .  /
+ .  . (.  .  .  .) .  .  /
+ .  .  .  .  .  .  .  .  /
+ P  P  P  P  P  P  P  P  /
+ R  N  B  Q  K  B  N  R
 ```
 
 ### TODO:
+- Add option to print coordinates on the edge of the board
 - Add highlighting using coordinates
+- Read PGN
+    - Highlight the latest move
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,96 @@
 use std::env;
 
+// TODO: error handling
+// TODO: input validation
+// TODO: help
+
 fn main() {
     let args: Vec<String> = env::args().collect();
-    if args.len() != 2 {
-        println!("Takes one argument but {} was given", args.len() - 1);
-        return
-    }
-    let string =  &args[1];
 
-    let new_string = format(&string);
-
-    match new_string {
-        Ok(s) => { println!("{}", s); },
-        Err(e) => { println!("Parsing error: {}", e); }
+    // print starting position if no arguments
+    if args.len() == 1 {
+        println!(" r  n  b  q  k  b  n  r  /");
+        println!(" p  p  p  p  p  p  p  p  /");
+        println!(" .  .  .  .  .  .  .  .  /");
+        println!(" .  .  .  .  .  .  .  .  /");
+        println!(" .  .  .  .  .  .  .  .  /");
+        println!(" .  .  .  .  .  .  .  .  /");
+        println!(" P  P  P  P  P  P  P  P  /");
+        println!(" R  N  B  Q  K  B  N  R  ");
     }
+
+    // Handle inputs of one argument
+    else if args.len() == 2 {
+        // First check for help arguments
+        if args[1] == "h"
+        || args[1] == "-h"
+        || args[1] == "--help"
+        || args[1] == "help" {
+            todo!("help");
+        }
+
+        // Check that input is a valid coordinate
+        else if args[1].len() == 2
+        && valid_coord(&args[1]) {
+
+            let coordinates = args[1..].to_vec();
+            let mut indices: Vec<(u8, u8)> = Vec::new();
+            for c in coordinates {
+                println!("{}", c);
+                indices.extend(parse_an(&c));
+            }
+            print_range(&indices);
+
+        // Second argument is a fen string and no other arguments
+        } else {
+            let fen =  &args[1];
+            let pretty_fen = format(&fen);
+            match pretty_fen {
+                Ok(s) => { println!("{}", s); },
+                Err(e) => { println!("Parsing error: {}", e); }
+            }
+        }
+
+    // More than one argument means fen + n coordinates
+    } else {
+        let fen = &args[1..];
+        let coordinates = args[2..].to_vec();
+        let mut indices: Vec<(u8, u8)> = Vec::new();
+
+        for c in coordinates {
+            println!("{}", c);
+            indices.extend(parse_an(&c));
+        }
+        print_range(&indices);
+}
+}
+
+fn valid_coord(c: &str) -> bool {
+    true
+}
+
+fn parse_an(s: &str) -> Vec<(u8, u8)> {
+    vec![(match s.chars().nth(0).unwrap() {
+        'a' => 0,
+        'b' => 1,
+        'c' => 2,
+        'd' => 3,
+        'e' => 4,
+        'f' => 5,
+        'g' => 6,
+        'h' => 7,
+        _ => panic!(),
+    }, match s.chars().nth(1).unwrap() {
+        '1' => 0,
+        '2' => 1,
+        '3' => 2,
+        '4' => 3,
+        '5' => 4,
+        '6' => 5,
+        '7' => 6,
+        '8' => 7,
+        _ => panic!(),
+    })]
 }
 
 fn format(input: &str) -> Result<String, i32> {
@@ -45,3 +122,40 @@ fn format(input: &str) -> Result<String, i32> {
 
     Ok(s)
 }
+
+fn print_range(indices: &Vec<(u8, u8)>) {
+
+    let r1 = ['R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'];
+    let r7 = ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'];
+    let r3 = ['.', '.', '.', '.', '.', '.', '.', '.'];
+    let r4 = ['.', '.', '.', '.', '.', '.', '.', '.'];
+    let r5 = ['.', '.', '.', '.', '.', '.', '.', '.'];
+    let r6 = ['.', '.', '.', '.', '.', '.', '.', '.'];
+    let r2 = ['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'];
+    let r8 = ['r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'];
+
+    let b = [r1, r2, r3, r4, r5, r6, r7, r8];
+
+    for (i, r) in b.iter().rev().enumerate() {
+        for (j, s) in r.iter().enumerate() {
+            let mut highlight = false;
+            for h in indices {
+                if i as u8 == (7 - h.1) && j as u8 == h.0 {
+                    highlight = true;
+                }
+            }
+            match highlight {
+                true => print!("({})", s),
+                false => print!(" {} ", s),
+            }
+        }
+        if i != 7 {
+            println!(" /");
+        } else {
+            println!();
+        }
+    }
+
+}
+
+
